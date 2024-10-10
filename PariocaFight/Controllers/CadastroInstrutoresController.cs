920b9570aeb5e4302a211c09eeef823e59c7866f
Chris7ianp <christian.paulo.rj@gmail.com>
2024-10-10 11:13:51 -0300

    Adicionando a estrutura do cadastro dos instrutores.

---
+++ PariocaFight/Controllers/CadastroInstrutoresController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PariocaFight.Controllers
+{
+    public class CadastroInstrutoresController : Controller
+    {
+        private IConfiguration _config;
+        public CadastroInstrutoresController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        public ActionResult Index()
+        {
+            return View("~/Views/Cadastros/CadastroInstrutores.cshtml");
+        }
+
+    }
+}
---
+++ PariocaFight/VO/Instrutores.cs
@@ -0,0 +1,11 @@
+﻿namespace PariocaFight.VO
+{
+    public class Instrutores
+    {
+        public int instrutorId { get; set; }
+        public string? Nome { get; set; }
+        public string? Sobrenome { get; set; }
+        public string? Faixa { get; set; }
+        public string? ArteMarcial { get; set; }
+    }
+}
